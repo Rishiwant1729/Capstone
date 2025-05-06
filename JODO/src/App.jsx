886e44ba71dc23529bce6877d90ad6945c512dd4
RIshiwant Kumar Maurya <rishiwamtk143@gmail.com>
2025-05-06 17:23:29 +0530

    Basic layout

---
+++ JODO/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './pages/hom'
+
+function App() {
+
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ JODO/src/layout/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './navbar'
+import './index.css'
+
+function Index(props) {
+  return (
+    <div>
+        <Navbar />
+        <div className="main">
+            <div className="main-wrap">
+                {props.children}
+            </div>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Index
